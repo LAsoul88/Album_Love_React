f6048e1e82970579654b0800dba000e6b2968dd7
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-09-07 13:46:28 -0700

    set up controller routes in server, set up exports and common modules in all controller pages, created album show.ejs file

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+/* === External Modules === */
+
 const express = require('express');
+
+// module instance
 const app = express();
+
+// PORT
 const PORT = 3000;
+
+/* === Internal Modules === */
+const controllers = require('./controllers');
+
+/* === Routes === */
+app.use('/', controllers.auth);
+app.use('/users', controllers.user);
+app.use('/albums', controllers.album);
 
 app.listen(PORT, () => console.log(`Listening for some tunes on port:`, PORT));
