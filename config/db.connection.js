88c4d1b9e661bcf8f74c92f098e25e937708b55c
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-09-07 14:08:45 -0700

    database connection configured

---
+++ config/db.connection.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const connectionStr = 'mongodb://localhost:27017/albumlove';
+
+mongoose.connect(connectionStr, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+mongoose.connection.on('connected', () => {
+  console.log(`Mongoose connected to ${connectionStr}`);
+});
+
+mongoose.connection.on('error', (error) => {
+  console.log(`Mongoose connection error`, error);
+});
+
+mongoose.connection.on('disconnected', () => {
+  console.log(`Mongoose disconnected`);
+});
