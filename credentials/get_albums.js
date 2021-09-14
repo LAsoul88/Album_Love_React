eb32fdcf28e6141ca95dc8faae244a77e14c918c
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-09-14 15:45:21 -0700

    added responsive animations to nav bar, album name/images on index page

--- credentials/get_albums.js
+++ credentials/get_albums.js
@@ -4,15 +4,13 @@ const getToken = require('./spotify_auth');
 
 const getAlbums = async (arr) => {
   let url = `https://api.spotify.com/v1/albums?ids=`;
-  let idChain = ''
   for (let i = 0; i < arr.length; i++) {
     if (i === arr.length - 1) {
-      idChain += arr[i] + '&market=US'
+      url += arr[i] + '&market=US'
     } else {
-    idChain += arr[i] + '%2C';
+    url += arr[i] + '%2C';
   }
 }
-url += idChain;
 
 const token = await getToken();
 const targets = await axios.get(url, {
