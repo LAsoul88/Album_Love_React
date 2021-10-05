763809a65da14932c3b9ead5a1b79785551fb37f
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-10-05 14:18:50 -0700

    setting up separate frontend repo

--- album-love-express/utils/logger.js
+++
@@ -1,7 +0,0 @@
-const logger = (req, res, next) => {
-  console.log(`${req.url} : ${req.method} - ${new Date().toLocaleTimeString()}`);
-  console.log(req.session);
-  next();
-};
-
-module.exports = logger;
