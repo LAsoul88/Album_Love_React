5f8c76cbffd0591107ac817eb48a69c574795adc
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-10-15 23:08:57 -0700

    moved useEffect/api call to home container

--- src/Album/Home/HomeContainer.js
+++ src/Album/Home/HomeContainer.js
@@ -1,12 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import SearchBar from './SearchBar';
 
+
 const HomeContainer = () => {
+
+  console.log('coming home');
+
+  const [token, setToken] = useState('');
+  const [albums, setAlbums] = useState([]);
+
+  useEffect(() => {
+    
+    axios('https://accounts.spotify.com/api/token', {
+      headers: {
+        'Content-Type' : 'application/x-www-form-urlencoded',
+        'Authorization' : 'Basic ' + btoa('2f212092437640b08c1577de2c87a6b3:e587869e784943bd91f1ba87094b4067')
+      },
+      data: 'grant_type=client_credentials',
+      method: 'POST'
+    })
+    .then(tokenResponse => {
+      setToken(tokenResponse.data.access_token);
+
+      axios(`https://api.spotify.com/v1/search?q=${query}&type=album&market=US&limit=50`, {
+        method: 'GET',
+        headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token }
+      })
+      .then(albumResponse => {
+        console.log(albumResponse.data.albums.items);
+        setAlbums(albumResponse.data.albums.items);
+      })
+    })
+  }, []);
   
   return (
     <>
       <SearchBar />
-      <button>Log</button>
     </>
   )
 }
--- src/Album/Home/SearchBar.js
+++ src/Album/Home/SearchBar.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import AlbumCard from './AlbumCard';
 
@@ -11,37 +11,6 @@ const SearchBar = props => {
     event.preventDefault();
   }
 
-
-  const [token, setToken] = useState('');
-  const [albums, setAlbums] = useState([]);
-
-  useEffect(() => {
-    
-    axios('https://accounts.spotify.com/api/token', {
-      headers: {
-        'Content-Type' : 'application/x-www-form-urlencoded',
-        'Authorization' : 'Basic ' + btoa('2f212092437640b08c1577de2c87a6b3:e587869e784943bd91f1ba87094b4067')
-      },
-      data: 'grant_type=client_credentials',
-      method: 'POST'
-    })
-    .then(tokenResponse => {
-      setToken(tokenResponse.data.access_token);
-
-      axios(`https://api.spotify.com/v1/search?q=${query}&type=album&market=US&limit=50`, {
-        method: 'GET',
-        headers: { 'Authorization' : 'Bearer ' + tokenResponse.data.access_token }
-      })
-      .then(albumResponse => {
-        console.log(albumResponse.data.albums.items);
-        setAlbums(albumResponse.data.albums.items);
-      })
-    })
-  }, []);
-
-
-
-
   return (
     <div>
       Album Search
@@ -52,14 +21,6 @@ const SearchBar = props => {
         />
         <button>Submit</button>
       </form>
-      <div className="albums__container">
-        {albums.map(album => (
-          <div key={album.name}>
-            <img src={album.images[0].url} alt={album.name} />
-            <h3>{album.name}</h3>
-          </div>
-        ))}
-      </div>
     </div>
   )
 }
