8552b3ac941127817e501db10fb482841d8f51aa
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-10-22 10:08:21 -0700

    destructured props in AlbumCard, set up link infrastructure for album name and images

--- src/Album/Home/AlbumCard.js
+++ src/Album/Home/AlbumCard.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './AlbumCard.css';
 
 const AlbumCard = ({ album }) => {
+  const { id, images, name } = album
+  const albumLink = `/:${id}`
   return (
     <div className="album__container">
-      <h3>{album.name}</h3>
+      <h3><a href={albumLink}>{name}</a></h3>
       <div>
-        <img src={album.images[0].url} alt={album.name} />
+        <a href={albumLink}><img src={images[0].url} alt={name} /></a>
       </div>
     </div>
   )
